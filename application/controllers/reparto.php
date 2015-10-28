af1da7fa4cb510f43541e650bc97988886a4a22d
gvera85 <gonzalovera85@gmail.com>
2015-10-27 23:07:44 -0300

    Se agrega ID a la planificación de viajes

--- application/controllers/reparto.php
+++ application/controllers/reparto.php
@@ -30,7 +30,7 @@ class Reparto extends CI_Controller{
    
     $this->grocery_crud->set_subject('Viaje');
     $this->grocery_crud->required_fields('id_proveedor');
-    $this->grocery_crud->columns('numero_de_viaje','id_proveedor','fecha_estimada_salida','fecha_estimada_llegada','patente_semi','patente_camion','id_empresa_transportista','id_estado','cantidad_productos');
+    $this->grocery_crud->columns('id', 'numero_de_viaje','id_proveedor','fecha_estimada_salida','fecha_estimada_llegada','patente_semi','patente_camion','id_empresa_transportista','id_estado','cantidad_productos');
     
     $this->grocery_crud->change_field_type('id_distribuidor','invisible');
     
